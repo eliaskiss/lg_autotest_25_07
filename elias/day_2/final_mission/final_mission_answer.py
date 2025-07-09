7b11d6a65e477db2f871fc3705bf157e9da01553
elias.kim <elias.kim@lge.com>
2025-07-09 17:16:27 +0900

    0709

--- elias/day_2/final_mission/final_mission_answer.py
+++ elias/day_2/final_mission/final_mission_answer.py
@@ -26,7 +26,7 @@ from openpyxl.styles.fonts import Font
 from openpyxl.styles.borders import Border, Side
 
 # Our modules
-sys.path.append('../../day_1/lec_pymysql')
+sys.path.append('../../day_3/lec_pymysql')
 sys.path.append('../../day_1/lec_pyserial')
 
 from lec_pymysql import Database
@@ -164,6 +164,7 @@ class MainDialog(QDialog):
             self.my_thread.progressUpdateSignal.connect(self.process_update)
             self.my_thread.start()
 
+
     def find_excel(self):
         file_name, _ = QFileDialog.getOpenFileName(self, 'Open file', './')
 
@@ -173,10 +174,114 @@ class MainDialog(QDialog):
         else:
             QMessageBox.warning(self, '경고', '파일이 선택되지 않았습니다!!!', QMessageBox.Yes)
 
+    def put_data_to_db(self):
+        wb = load_workbook(self.excel_file_name)
+        ws = wb.active
+
+        db = Database(DB_URL, DB_USER, DB_PW, DB_NAME)
+        db.connect_db()
+
+        columns = ['station_number', 'station_name', 'region', 'address', 'latitude', 'longitude',
+                   'install_date', 'lcd_count', 'qr_count', 'proc_type']
+
+        for row in ws.iter_rows(min_row=6):
+            sql = f'insert into elias_bicycle ({",".join(columns)}) values({("%s," * len(columns))[:-1]})'
+            values = [_row.value for _row in row]
+            db.execute_only(sql, values)
+
+        db.commit_only()
+
+    # todo
     def get_data_from_db(self, from_date, region, output_file_name):
         # todo : from_date 이후에 region에서 설치된 자전거 데이터를 output_file_name에 엑셀로 저장
         # ...
-        pass
+        wb = Workbook()
+        ws = wb.active
+
+        ws.title = '공유자전거'
+
+        ws['A1'] = '대여소번호'
+        ws.merge_cells('A1:A5')
+
+        ws['B1'] = '보관소(대여소)명'
+        ws.merge_cells('B1:B5')
+
+        ws['C1'] = '소재자(위치)'
+        ws.merge_cells('C1:F2')
+
+        ws['C3'] = '자치구'
+        ws.merge_cells('C3:C5')
+
+        ws['D3'] = '상세주소'
+        ws.merge_cells('D3:D5')
+
+        ws['E3'] = '위도'
+        ws.merge_cells('E3:E5')
+
+        ws['F3'] = '경도'
+        ws.merge_cells('F3:F5')
+
+        ws['G1'] = '설치시기'
+        ws.merge_cells('G1:G5')
+
+        ws['H1'] = '설치형태'
+        ws.merge_cells('H1:I1')
+
+        ws['H2'] = 'LCD'
+        ws.merge_cells('H2:H3')
+
+        ws['H4'] = '거치대수'
+        ws.merge_cells('H4:H5')
+
+        ws['I2'] = 'QR'
+        ws.merge_cells('I2:I3')
+
+        ws['I4'] = '거치대수'
+        ws.merge_cells('I4:I5')
+
+        ws['J1'] = '운영방식'
+        ws.merge_cells('J1:J5')
+
+        db = Database(DB_URL, DB_USER, DB_PW, DB_NAME)
+        db.connect_db()
+
+        sql = 'SELECT * FROM elias_bicycle WHERE DATE(install_date) >= %s AND region = %s;'
+        values = (from_date, region)
+        data_list = db.execute_and_return(sql, values)
+
+        for data in data_list:
+            ws.append([data['station_number'], data['station_name'], data['region'],
+                       data['address'], data['latitude'], data['longitude'],
+                       data['install_date'], data['lcd_count'], data['qr_count'],
+                       data['proc_type']])
+
+        thin_border = Border(left=Side(style='thin'),
+                             right=Side(style='thin'),
+                             top=Side(style='thin'),
+                             bottom=Side(style='thin'))
+
+        bold_white_font = Font(bold=True,
+                               size=12,
+                               italic=None,
+                               underline=None,
+                               strike=None,
+                               color='FFFFFF')
+
+        background_fill = PatternFill(start_color='525E75',
+                                      end_color='525E75',
+                                      fill_type='solid')
+
+        for row in ws.iter_rows(max_row=5):
+            for cell in row:
+                cell.fill = background_fill
+                cell.font = bold_white_font
+
+        for col in ws.columns:
+            for cell in col:
+                cell.alignment = Alignment(horizontal='center', vertical='center')
+                cell.border = thin_border
+
+        wb.save(output_file_name)
 
     def clear_log(self):
         self.main_ui.tb_log_power.clear()
@@ -184,12 +289,42 @@ class MainDialog(QDialog):
     def set_power(self):
         # todo : rb_on 컨트롤의 상태를 체크하여 Power On(b'ka 01 01') or Power Off(b'ka 01 00') 명령어를 전송하고
         #        응답값을 받아 로그에 추가(add_log)
-        pass
+        is_power_on = self.main_ui.rb_on.isChecked()
+
+        if is_power_on:
+            command = b'ka 01 01\r'
+        else:
+            command = b'ka 01 00\r'
+
+        self.add_log('=' * 30)
+        self.add_log(f'Send Command: {command.decode()[:-1]}')
+        self.ser.writePort(command)
+        resp = self.ser.readLine()
+        self.add_log(f'Response: {resp.decode()[:-1]}')
 
     def get_power(self):
         # todo : 전원상태를 가져오는 명령어(b'ka 01 ff')를 전송하여, 전원상태에 맞게 rb_on, rb_off 라디오 버튼 선택
         # ...
-        pass
+        command = b'ka 01 ff\r'
+
+        self.add_log('=' * 30)
+        self.add_log(f'Send Command: {command.decode()[:-1]}')
+        self.ser.writePort(command)
+        resp = self.ser.readLine()
+        self.add_log(f'Response: {resp.decode()[:-1]}')
+
+        # b'a 01 OK01x\r'
+        # 'a 01 OK01x'
+        # [7:-2] --> 01
+        resp = resp[:-1].decode()
+        power_state = resp[7:-1]
+
+        if power_state == '01':
+            self.main_ui.rb_on.setChecked(True)
+            self.main_ui.rb_off.setChecked(False)
+        else:
+            self.main_ui.rb_on.setChecked(False)
+            self.main_ui.rb_off.setChecked(True)
 
     @pyqtSlot()
     def thread_is_stopped(self):
@@ -226,7 +361,25 @@ class UploadThread(QThread):
         # todo: 엑셀파일(self.excel_file_name)을 읽어서, DB에 업데이트
         #       self.progressInitSignal.emit()을 이용해서 progress max값 설정
         #       self.progressUpdateSignal.emit()을 이용해서 진행상황겂 업데이트
-        pass
+        wb = load_workbook(self.excel_file_name)
+        ws = wb.active
+
+        db = Database(DB_URL, DB_USER, DB_PW, DB_NAME)
+        db.connect_db()
+
+        columns = ['station_number', 'station_name', 'region', 'address', 'latitude', 'longitude',
+                   'install_date', 'lcd_count', 'qr_count', 'proc_type']
+
+        total_length = len(list(ws.iter_rows(min_row=6)))
+        self.progressInitSignal.emit(total_length)
+
+        for index, row in enumerate(ws.iter_rows(min_row=6)):
+            sql = f'insert into elias_bicycle ({",".join(columns)}) values({("%s," * len(columns))[:-1]})'
+            values = [_row.value for _row in row]
+            db.execute_only(sql, values)
+            self.progressUpdateSignal.emit(index+1)
+
+        db.commit_only()
 
     def run(self):
         self.put_data_to_db()
@@ -235,6 +388,7 @@ class UploadThread(QThread):
     def stop(self):
         self.isRunning = False
         self.logSignal.emit('Thread is stopping')
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
